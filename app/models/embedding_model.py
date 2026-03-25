e024523be589f6066b6a975fe58c4fa8a2300ba1
yanadre <yannadrevin@gmail.com>
2026-03-25 20:58:46 +0200

    fixed StubEmbeddingModel: now inherits form langchain.embeddings.base.Embeddings and implements embed_query and embed_documents

--- app/models/embedding_model.py
+++ app/models/embedding_model.py
@@ -1,16 +1,18 @@
 from typing import List
 import random
+from langchain.embeddings.base import Embeddings
 
-class StubEmbeddingModel:
+
+class StubEmbeddingModel(Embeddings):
     def __init__(self, vector_size: int = 768):
         self.vector_size = vector_size
 
-    def embed(self, text: str) -> List[float]:
+    def embed_query(self, text: str) -> List[float]:
         """Return a dummy vector for testing"""
         return [random.random() for _ in range(self.vector_size)]
 
-    def embed_batch(self, texts: List[str]) -> List[List[float]]:
-        return [self.embed(t) for t in texts]
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        return [self.embed_query(t) for t in texts]
     
 
 if __name__ == "__main__":
@@ -19,6 +21,6 @@ if __name__ == "__main__":
     stub_embedding_model = StubEmbeddingModel(vector_size=768)
     my_doc = "bot Yaana"
     my_docs = ["a", "b", "c", "d"]
-    embeded_doc = stub_embedding_model.embed(text=my_doc)
-    embedded_docs = stub_embedding_model.embed_batch(my_docs)
+    embeded_doc = stub_embedding_model.embed_query(text=my_doc)
+    embedded_docs = stub_embedding_model.embed_documents(my_docs)
     print()
