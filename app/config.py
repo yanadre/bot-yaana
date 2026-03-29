8508369ff9b9b916e1926e838741018a1441a0eb
yanadre <yannadrevin@gmail.com>
2026-03-29 21:09:27 +0300

    updated config

--- app/config.py
+++ app/config.py
@@ -7,10 +7,17 @@ class Settings(BaseSettings):
     # qdrant
     QDRANT_HOST: str 
     QDRANT_PORT: int  
-    QDRANT_COLLECTION_NAME: str = "documents"   
+    QDRANT_COLLECTION_NAME: str = "documents_tests"   
 
     # telegram
-    TELEGRAM_TOKEN: str      
+    TELEGRAM_TOKEN: str     
+    AUTHORIZED_ID: str 
+
+    # embedding
+    GOOGLE_API_KEY: str
+    EMBEDDING_MODEL_NAME: str = "gemini-embedding-001"
+    EMBEDDING_VECTOR_SIZE: int = 3072
+    
     
     
     
